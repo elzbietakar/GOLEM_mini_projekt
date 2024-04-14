ea7840d3348ab838f25e99b8754e916595e37cbc
zuzawaszczuk <zuza.waszczuk@gmail.com>
2024-04-14 20:55:55 +0200

    Function eval

---
+++ eval.py
@@ -0,0 +1,17 @@
+import torch
+
+def eval(model, criterion, loader):
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for data in loader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = model(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
