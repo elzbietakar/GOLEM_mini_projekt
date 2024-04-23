be0a9244a394551f2ac3582cd345b481fd6b86ea
zuzawaszczuk <zuza.waszczuk@gmail.com>
2024-04-23 13:15:12 +0200

    New model trained 84% accuracy, little dropout, not overfitted small"UNET" 512channels

--- model2_v3.py
+++ model2_v3.py
@@ -6,35 +6,35 @@ class ZuziaNet2(nn.Module):
     def __init__(self):
         super().__init__()
         self.block1 = nn.Sequential(
-             nn.Conv2d(3, 16, kernel_size=6, bias=False),
-             nn.BatchNorm2d(16),
-             nn.ReLU(),
-             nn.Dropout2d(p=0.2),
-        )
-        self.block2 = nn.Sequential(
-             nn.Conv2d(16, 64, kernel_size=3, bias=False),
-             nn.BatchNorm2d(64),
-             nn.ReLU(),
-             nn.MaxPool2d(2, 2),
-             nn.Dropout2d(p=0.2)
-        )
-        self.block3 = nn.Sequential(
-             nn.Conv2d(64,128, kernel_size=3, bias=False),
-             nn.BatchNorm2d(128),
-             nn.ReLU(),
-             nn.Dropout2d(p=0.2),
-        )
-        self.block4 = nn.Sequential(
-             nn.Conv2d(128, 64, kernel_size=3, bias=False),
-             nn.BatchNorm2d(64),
-             nn.ReLU(),
-             nn.Dropout2d(p=0.2)
-        )
-        self.block5 = nn.Sequential(
-             nn.Conv2d(64, 32, kernel_size=3, bias=False),
+             nn.Conv2d(3, 32, kernel_size=6, bias=False),
              nn.BatchNorm2d(32),
              nn.ReLU(),
-             nn.Dropout2d(p=0.2),
+             nn.Dropout2d(p=0.1),
+        )
+        self.block2 = nn.Sequential(
+             nn.Conv2d(32, 128, kernel_size=3, bias=False),
+             nn.BatchNorm2d(128),
+             nn.ReLU(),
+             nn.MaxPool2d(2, 2),
+             nn.Dropout2d(p=0.1)
+        )
+        self.block3 = nn.Sequential(
+             nn.Conv2d(128,512, kernel_size=3, bias=False),
+             nn.BatchNorm2d(512),
+             nn.ReLU(),
+             nn.Dropout2d(p=0.1),
+        )
+        self.block4 = nn.Sequential(
+             nn.Conv2d(512, 128, kernel_size=3, bias=False),
+             nn.BatchNorm2d(128),
+             nn.ReLU(),
+             nn.Dropout2d(p=0.1)
+        )
+        self.block5 = nn.Sequential(
+             nn.Conv2d(128, 32, kernel_size=3, bias=False),
+             nn.BatchNorm2d(32),
+             nn.ReLU(),
+             nn.Dropout2d(p=0.1),
         )
         self.fc1 = nn.Linear(32 * 6 * 6, 10)
 
