05fe2da34bafe7eb620163d2411e7d8db89b0d9b
aszczypa <01187290@pw.edu.pl>
2024-04-24 23:53:19 +0200

    Uploaded models basic

--- basic_upgrade.py
+++ basic_upgrade.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+
 class ZuziaNet(nn.Module):
     def __init__(self):
         super().__init__()
--- model_basic_2.py
+++ model_basic_2.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 
 
-class OlaNet(nn.Module):
+class OlaNet2(nn.Module):
     def __init__(self):
         super().__init__()
         self.block1 = nn.Sequential(
@@ -10,20 +10,17 @@ class OlaNet(nn.Module):
             nn.BatchNorm2d(16),
             nn.ReLU(),
         )
-        # 32 -3 +1 = 30
         self.block2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=3, bias=False),
             nn.ReLU(),
-            nn.MaxPool2d(2,2)
+            nn.MaxPool2d(2, 2)
         )
-        # 30 - 2 = 28 -2/2 +1 = 14
         self.block3 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2,2)
+            nn.MaxPool2d(2, 2)
         )
-        # 14 - 2 = 12 -2/2 +1 = 6
 
         self.fc1 = nn.Linear(64 * 6 * 6, 512)
         self.fc2 = nn.Linear(512, 10)
@@ -32,7 +29,6 @@ class OlaNet(nn.Module):
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
-
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = self.fc2(x)
