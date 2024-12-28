c811921e654dd5db375779c987b2324bb9413156
zuzawaszczuk <zuza.waszczuk@gmail.com>
2024-12-28 01:38:29 +0100

    scripts

--- check_parameters.py
+++ check_parameters.py
@@ -1,5 +1,5 @@
 from small_model import OlaNet4
-from resnet_model import ResNet
+from resnet_fine_tuning import ResNet
 from big_model_with_dropout import ZuziaNet
 from best_model import ZuziaNet2
 
@@ -8,9 +8,11 @@ znet = ZuziaNet()
 znet2 = ZuziaNet2()
 resnet = ResNet()
 
+
 def parameters(model):
     params = sum(p.numel() for p in model.parameters())
     print(params)
+
 
 parameters(onet)
 parameters(znet)
--- script.py
+++ script.py
@@ -5,38 +5,38 @@ from eval import eval
 import torch.nn as nn
 import torch.optim as optim
 from train_epoch import train_epoch
-from big_model_with_dropout import ZuziaNet
+from small_model import OlaNet4
 import matplotlib.pyplot as plt
 import os
 
 train_transform = transforms.Compose([
-        transforms.Resize(224),
+        # transforms.Resize(224),
         transforms.RandomRotation(20),
         transforms.RandomHorizontalFlip(0.1),
-        transforms.ColorJitter(brightness = 0.1,contrast = 0.1 ,saturation =0.1 ),
-        transforms.RandomAdjustSharpness(sharpness_factor = 2, p = 0.1),
+        transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+        transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.1),
         transforms.ToTensor(),
-        transforms.RandomErasing(p=0.75,scale=(0.02, 0.1),value=1.0, inplace=False),
+        transforms.RandomErasing(p=0.75, scale=(0.02, 0.1), value=1.0, inplace=False),
         transforms.Normalize(mean=(0.4914, 0.4822, 0.4465),  std=(0.247, 0.243, 0.261))
     ])
 
 test_transform = transforms.Compose([
-        transforms.Resize(224),
+        # transforms.Resize(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=(0.4914, 0.4822, 0.4465),  std=(0.247, 0.243, 0.261))
     ])
 
 
 # How many pictures are analyzed in one iteration
-batch_size = 256
+batch_size = 32
 
 # Defining datasets and dataloaders for train and test data
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+trainset = torchvision.datasets.CIFAR10(root='./data10', train=True,
                                         download=True, transform=train_transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+testset = torchvision.datasets.CIFAR10(root='./data10', train=False,
                                        download=True, transform=test_transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False)
@@ -46,8 +46,7 @@ classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-
-znet = ZuziaNet()
+znet = OlaNet4()
 
 if torch.cuda.is_available():
     znet.cuda()
@@ -62,7 +61,8 @@ eval_loss = []
 metrics = []
 epochs = []
 
-how_many_epoch = 50
+how_many_epoch = 2
+
 for epoch in range(how_many_epoch):
     tloss = train_epoch(znet, criterion, optimizer, trainloader, epoch)
     eloss, classification_report = eval(znet, criterion, testloader)
@@ -76,7 +76,7 @@ print(train_loss)
 print(eval_loss)
 print(metrics)
 
-PATH = f"zresnet_fine_tuning_224_{how_many_epoch}epoch_SGD_{batch_size}"
+PATH = f"small_model_{how_many_epoch}epoch_Adam_{batch_size}"
 if not os.path.exists(PATH):
     os.makedirs(PATH)
 
@@ -89,8 +89,8 @@ plt.ylabel('Loss')
 
 plt.legend()
 
-plt.savefig(f"{PATH}\plot_loss.png")
-plt.show()
+plt.savefig(f"{PATH}/plot_loss.png")
+plt.clf()
 
 accuracy = [report[0] for report in metrics]
 plt.plot(epochs, accuracy, label='Accuracy')
@@ -101,8 +101,8 @@ plt.ylabel('Metrics')
 plt.grid(True)
 plt.legend()
 
-plt.savefig(f"{PATH}\plot_metrics.png")
+plt.savefig(f"{PATH}/plot_metrics.png")
 plt.show()
 
-#Saves model
-torch.save(znet.state_dict(),(f"{PATH}\model.pth"))
+# Saves model
+torch.save(znet.state_dict(), (f"{PATH}/model.pth"))
